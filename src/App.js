ceb3158d1d5d3217ce405100ee00a64bfdf23f40
deserkhalifa <Deserkhalifa18@gmail.com>
2025-05-28 15:51:33 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>UC Clicker App 🚀</h1>
+      <p>Click to earn points and exchange them for PUBG UC!</p>
+    </div>
+  );
+}
+
+export default App;
